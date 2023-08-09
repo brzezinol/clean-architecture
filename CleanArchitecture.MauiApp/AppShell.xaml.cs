be5e22fafe5cd53cb7c612b34e0d4c064c093e42
Piotr Brzezinski <p.brzezinski@listprzewozowy.com.pl>
2023-08-09 21:27:02 +0200

    Setup

---
+++ CleanArchitecture.MauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.MauiApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CleanArchitecture.MauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CleanArchitecture.MauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
